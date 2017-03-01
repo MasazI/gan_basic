7a77dea002a1accd02552508cecdac8d7a124f31
MasazI <masazdream@gmail.com>
2017-03-01 18:27:29 +0900

    add part of model

---
+++ mnist/model_part.py
@@ -0,0 +1,89 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+TOWER_NAME = 'tower'
+UPDATE_OPS_COLLECTION = '_update_ops_'
+
+
+def _variable_with_weight_decay(name, shape, stddev, wd, trainable=True):
+    var = _variable_on_gpu(name, shape, tf.truncated_normal_initializer(stddev=stddev))
+    if wd:
+        weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
+        tf.add_to_collection('losses', weight_decay)
+    return var
+
+
+def _variable_on_gpu(name, shape, initializer):
+    var = tf.get_variable(name, shape, initializer=initializer)
+    return var
+
+
+def conv2d(scope_name, inputs, shape, bias_shape, stride, padding='VALID', wd=0.0, reuse=False, trainable=True):
+    with tf.variable_scope(scope_name) as scope:
+        if reuse is True:
+            scope.reuse_variables()
+        kernel = _variable_with_weight_decay(
+            'weights',
+            shape=shape,
+            stddev=0.01,
+            wd=wd,
+            trainable=trainable
+        )
+        conv = tf.nn.conv2d(inputs, kernel, stride, padding=padding)
+        biases = _variable_on_gpu('biases', bias_shape, tf.constant_initializer(0.1))
+        bias = tf.nn.bias_add(conv, biases)
+        conv_ = tf.nn.relu(bias, name=scope.name)
+        return conv_
+
+
+def fc(scope_name, inputs, shape, bias_shape, wd=0.04, reuse=False, trainable=True):
+    with tf.variable_scope(scope_name) as scope:
+        if reuse is True:
+            scope.reuse_variables()
+        # weights = _variable_with_weight_decay(
+        #     'weights',
+        #     shape,
+        #     stddev=0.1,
+        #     wd=wd,
+        #     trainable=trainable
+        # )
+        weights = _variable_on_gpu('weights', shape,tf.random_normal_initializer())
+        biases = _variable_on_gpu('biases', bias_shape, tf.constant_initializer(0.0))
+        fc = tf.nn.tanh(tf.add(tf.matmul(inputs, weights), biases, name=scope.name))
+        return fc, weights, biases
+
+
+def fc_relu(scope_name, inputs, shape, bias_shape, wd=0.04, reuse=False, trainable=True):
+    with tf.variable_scope(scope_name) as scope:
+        if reuse is True:
+            scope.reuse_variables()
+        weights = _variable_with_weight_decay(
+            'weights',
+            shape,
+            stddev=0.01,
+            wd=wd,
+            trainable=trainable
+        )
+        biases = _variable_on_gpu('biases', bias_shape, tf.constant_initializer(0.1))
+        fc = tf.nn.relu_layer(inputs, weights, biases, name=scope.name)
+        return fc, weights, biases
+
+
+def fc_softmax(scope_name, inputs, shape, bias_shape, wd=0.04, reuse=False, trainable=True):
+    with tf.variable_scope(scope_name) as scope:
+        if reuse is True:
+            scope.reuse_variables()
+        flat = tf.reshape(inputs, [-1, shape[0]])
+        weights = _variable_with_weight_decay(
+            'weights',
+            shape,
+            stddev=0.01,
+            wd=wd,
+            trainable=trainable
+        )
+        biases = _variable_on_gpu('biases', bias_shape, tf.constant_initializer(0.1))
+        softmax_linear = tf.add(tf.matmul(flat, weights), biases, name=scope.name)
+        return softmax_linear, weights, biases
