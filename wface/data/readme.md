60ba6562f94c8dfc5a7ee2dd55f183a6ae85933e
MasazI <masazdream@gmail.com>
2017-04-04 16:13:47 +0900

    add wasserstein.

---
+++ wface/data/readme.md
@@ -0,0 +1,1 @@
+face download: https://s3-us-west-2.amazonaws.com/deep-learning.datasection.co.jp/datasets/face.tar.gz
---
+++ wface/sampling_from_vec.py
@@ -0,0 +1,108 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import numpy as np
+import model
+import tensorflow as tf
+from tensorflow.python.platform import gfile
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer("z_dim_v", 100, "dimension of dim for Z for sampling")
+flags.DEFINE_integer("gc_dim_v", 64, "dimension of generative filters in conv layer")
+flags.DEFINE_integer("dc_dim_v", 64, "dimension of discriminative filters in conv layer")
+
+flags.DEFINE_string("model_name_v", "face", "model_name")
+flags.DEFINE_string("data_dir_v", "data/face", "data dir path")
+flags.DEFINE_string("sample_dir_v", "samples", "sample_name")
+flags.DEFINE_string("checkpoint_dir_v", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
+flags.DEFINE_integer("batch_size_v", 1, "The size of batch images [64]")
+flags.DEFINE_float('gpu_memory_fraction_v', 0.5, 'gpu memory fraction.')
+flags.DEFINE_string("sample_type_v", "type1", "sample type name.")
+
+class DCGAN_S():
+    def __init__(self, model_name, checkpoint_dir):
+        self.model_name = model_name
+        self.checkpoint_dir = checkpoint_dir
+
+    def step(self, z):
+        z_sum = tf.summary.histogram("z", z)
+
+        self.generator = model.Generator(FLAGS.batch_size_v, FLAGS.gc_dim_v)
+        self.G = self.generator.inference(z)
+
+        # descriminator inference using true images
+        self.discriminator = model.Descriminator(FLAGS.batch_size_v, FLAGS.dc_dim_v)
+        #self.D1, D1_logits = self.discriminator.inference(images)
+
+        # descriminator inference using sampling with G
+        self.samples = self.generator.sampler(z, reuse=True, trainable=False)
+        self.D2, D2_logits = self.discriminator.inference(self.G, reuse=False)
+
+#        d1_sum = tf.summary.histogram("d1", self.D1)
+        d2_sum = tf.summary.histogram("d2", self.D2)
+        G_sum = tf.summary.histogram("G", self.G)
+
+        # return images, D1_logits, D2_logits, G_sum, z_sum, d1_sum, d2_sum
+        # return D2_logits, G_sum, z_sum, d1_sum, d2_sum
+        return D2_logits, G_sum, z_sum, d2_sum
+
+    def generate_images(self, z, row=8, col=8):
+        images = tf.cast(tf.multiply(tf.add(self.G, 1.0), 127.5), tf.uint8)
+        print(images.get_shape())
+        images = [image for image in tf.split(images, FLAGS.batch_size_v, axis=0)]
+        rows = []
+        for i in range(row):
+            rows.append(tf.concat(images[col * i + 0:col * i + col], axis=2))
+        image = tf.concat(rows, axis=1)
+        return tf.image.encode_png(tf.squeeze(image, [0]))
+
+def sampling(z_eval):
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=FLAGS.gpu_memory_fraction_v)
+    with tf.Session(config=tf.ConfigProto(
+        allow_soft_placement=True,
+        gpu_options=gpu_options)) as sess:
+        dcgan = DCGAN_S(FLAGS.model_name_v, FLAGS.checkpoint_dir_v)
+        z = tf.placeholder(tf.float32, [None, FLAGS.z_dim_v], name='z')
+
+        # build model
+        dcgan.step(z)
+        generate_images = dcgan.generate_images(z, 1, 1)
+
+        # saver
+        t_vars = tf.trainable_variables()
+        g_vars = [var for var in t_vars if 'g_' in var.name]
+        saver = tf.train.Saver(g_vars)
+
+        # create session
+        sess.run(tf.global_variables_initializer())
+
+        # load parameters
+        model_dir = os.path.join(FLAGS.model_name_v, FLAGS.checkpoint_dir_v)
+        ckpt = tf.train.get_checkpoint_state(model_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            print("No checkpoint file found")
+            exit()
+        print("Model restored.")
+
+        generated_image_eval = sess.run(generate_images, {z: z_eval})
+        out_dir = os.path.join(FLAGS.model_name_v, FLAGS.sample_dir_v)
+        if not gfile.Exists(out_dir):
+            gfile.MakeDirs(out_dir)
+        filename = os.path.join(out_dir, 'sampling_%s.png' % (FLAGS.sample_type_v))
+        with open(filename, 'wb') as f:
+            f.write(generated_image_eval)
+
+def main(_):
+    print("face DCGANs sampling.")
+    sampling()
+
+
+if __name__ == '__main__':
+    tf.app.run()
+
+
