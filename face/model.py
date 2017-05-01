e27147ce623e5874669c853224c1fa8777895454
MasazI <masazdream@gmail.com>
2017-05-01 16:50:11 +0900

    add generator expand for face

--- face/model.py
+++ face/model.py
@@ -6,6 +6,185 @@ import tensorflow as tf
 import model_part as mp
 
 
+class GeneratorExpand:
+    def __init__(self, batch_size, first_conv_dim):
+        self.batch_size = batch_size
+        self.first_conv_dim = first_conv_dim
+        self.fc_dim = 8192
+
+        self.g_bn0 = mp.BatchNorm(name='g_bn0')
+        self.g_bn1 = mp.BatchNorm(name='g_bn1')
+        self.g_bn2 = mp.BatchNorm(name='g_bn2')
+        self.g_bn3 = mp.BatchNorm(name='g_bn3')
+        self.g_bn1_2 = mp.BatchNorm(name='g_bn1_2')
+        self.g_bn2_2 = mp.BatchNorm(name='g_bn2_2')
+        self.g_bn3_2 = mp.BatchNorm(name='g_bn3_2')
+
+    def sampler(self, z, reuse=True, trainable=True):
+        with tf.variable_scope("generator") as scope:
+            if reuse:
+                scope.reuse_variables()
+            print("===G")
+            # linear projection.
+            z_, h0_w, self.h0_b = mp.linear_project('g_lin_project_h0', z, self.first_conv_dim * 8 * 4 * 4, reuse=reuse, with_w=True)
+
+            g_fc1, w1, b1 = mp.fc_relu('g_fc1', z_, [z_.get_shape()[1], self.fc_dim], [self.fc_dim], reuse=reuse, trainable=trainable)
+            # reshape for cnn inputs.
+            h0 = tf.reshape(g_fc1, [-1, 4, 4, self.first_conv_dim * 8])
+            # batch norm
+            h0 = tf.nn.relu(self.g_bn0(h0, trainable=trainable))
+            # h0 = tf.nn.relu(mp.batch_norm(h0, scope_name='g_bn_h0', reuse=reuse, trainable=trainable))
+
+            # deconv1 conv2d_transpose arguments = (scope_name, inputs, shape, output_shape, bias_shape, stride, padding='VALID', reuse=False, trainable=True)
+            deconv_h1, deconv_h1_w, deconv_h1_b = mp.conv2d_transpose('g_deconv_h1', h0,
+                                                                      [5, 5, self.first_conv_dim * 8, h0.get_shape()[-1]],
+                                                                      [self.batch_size, 8, 8, self.first_conv_dim * 8],
+                                                                      [self.first_conv_dim * 8], [1, 2, 2, 1],
+                                                                      padding='SAME', reuse=reuse,  with_w=True)
+            h1 = tf.nn.relu(self.g_bn1(deconv_h1, trainable=trainable))
+            # h1 = tf.nn.relu(mp.batch_norm(deconv_h1, scope_name='g_bn_h1', reuse=reuse, trainable=trainable))
+
+            # 1st conv
+            conv_h1_2, conv_h1_2_w, conv_h1_2_b = mp.conv2d('g_conv_h1_2', h1,
+                                                            [5, 5, h1.get_shape()[-1], self.first_conv_dim * 8],
+                                                            [self.first_conv_dim * 8],
+                                                            [1, 1, 1, 1],
+                                                            padding='SAME', reuse=reuse, with_w=True,
+                                                            trainable=trainable)
+            h1_2 = tf.nn.relu(self.g_bn1_2(conv_h1_2, trainable=trainable))
+
+            # deconv2 conv2d_transpose arguments = (scope_name, inputs, shape, output_shape, bias_shape, stride, padding='VALID', reuse=False, trainable=True)
+            deconv_h2, deconv_h2_w, deconv_h2_b = mp.conv2d_transpose('g_deconv_h2', h1_2,
+                                                                      [5, 5, self.first_conv_dim * 4, h1_2.get_shape()[-1]],
+                                                                      [self.batch_size, 16, 16, self.first_conv_dim * 4],
+                                                                      [self.first_conv_dim * 4], [1, 2, 2, 1],
+                                                                      padding='SAME', reuse=reuse, with_w=True)
+            print(deconv_h2.get_shape())
+            h2 = tf.nn.relu(self.g_bn2(deconv_h2, trainable=trainable))
+            # h2 = tf.nn.relu(mp.batch_norm(deconv_h2, scope_name='g_bn_h2', reuse=reuse, trainable=trainable))
+
+            # 2nd conv
+            conv_h2_2, conv_h2_2_w, conv_h2_2_b = mp.conv2d('g_conv_h2_2', h2,
+                                                            [5, 5, h2.get_shape()[-1], self.first_conv_dim * 4],
+                                                            [self.first_conv_dim * 4],
+                                                            [1, 1, 1, 1],
+                                                            padding='SAME', reuse=reuse, with_w=True,
+                                                            trainable=trainable)
+            h2_2 = tf.nn.relu(self.g_bn2_2(conv_h2_2, trainable=trainable))
+
+            # 3rd
+            deconv_h3, deconv_h3_w, deconv_h3_b = mp.conv2d_transpose('g_deconv_h3', h2_2,
+                                                                      [5, 5, self.first_conv_dim * 2, h2_2.get_shape()[-1]],
+                                                                      [self.batch_size, 32, 32, self.first_conv_dim * 2],
+                                                                      [self.first_conv_dim * 2], [1, 2, 2, 1],
+                                                                      padding='SAME', reuse=reuse, with_w=True)
+            h3 = tf.nn.relu(self.g_bn3(deconv_h3, trainable=trainable))
+            # h3 = tf.nn.relu(mp.batch_norm(deconv_h3, scope_name='g_bn_h3', reuse=reuse, trainable=trainable))
+
+            # 3rd conv
+            conv_h3_2, conv_h3_2_w, conv_h3_2_b = mp.conv2d('g_conv_h3_2', h3,
+                                                            [5, 5, h3.get_shape()[-1], self.first_conv_dim * 2],
+                                                            [self.first_conv_dim * 2],
+                                                            [1, 1, 1, 1],
+                                                            padding='SAME', reuse=reuse, with_w=True,
+                                                            trainable=trainable)
+            h3_2 = tf.nn.relu(self.g_bn3_2(conv_h3_2, trainable=trainable))
+
+
+            # 4th
+            deconv_h4, deconv_h4_w, deconv_h4_b = mp.conv2d_transpose('g_deconv_h4', h3_2,
+                                                                      [5, 5, 3, h3_2.get_shape()[-1]],
+                                                                      [self.batch_size, 64, 64, 3],
+                                                                      [3], [1, 2, 2, 1],
+                                                                      padding='SAME', reuse=reuse, with_w=True)
+            return tf.nn.tanh(deconv_h4)
+
+
+    def inference(self, z, reuse=False, trainable=True):
+        with tf.variable_scope("generator") as scope:
+            if reuse:
+                scope.reuse_variables()
+            print("===G")
+            # linear projection.
+            z_, h0_w, self.h0_b = mp.linear_project('g_lin_project_h0', z, self.first_conv_dim * 8 * 4 * 4, reuse=reuse, with_w=True)
+
+            g_fc1, w1, b1 = mp.fc_relu('g_fc1', z_, [z_.get_shape()[1], self.fc_dim], [self.fc_dim], reuse=reuse,
+                                       trainable=trainable)
+            # reshape for cnn inputs.
+            h0 = tf.reshape(g_fc1, [-1, 4, 4, self.first_conv_dim * 8])
+            # batch norm
+            h0 = tf.nn.relu(self.g_bn0(h0, trainable=trainable))
+            # h0 = tf.nn.relu(mp.batch_norm(h0, scope_name='g_bn_h0', reuse=reuse, trainable=trainable))
+
+            # deconv1 conv2d_transpose arguments = (scope_name, inputs, shape, output_shape, bias_shape, stride, padding='VALID', reuse=False, trainable=True)
+            deconv_h1, deconv_h1_w, deconv_h1_b = mp.conv2d_transpose('g_deconv_h1', h0,
+                                                                      [5, 5, self.first_conv_dim * 8, h0.get_shape()[-1]],
+                                                                      [self.batch_size, 8, 8, self.first_conv_dim * 8],
+                                                                      [self.first_conv_dim * 8], [1, 2, 2, 1],
+                                                                      padding='SAME', reuse=reuse,  with_w=True, trainable=trainable)
+            h1 = tf.nn.relu(self.g_bn1(deconv_h1, trainable=trainable))
+            # h1 = tf.nn.relu(mp.batch_norm(deconv_h1, scope_name='g_bn_h1', reuse=reuse, trainable=trainable))
+
+            # 1st conv
+            conv_h1_2, conv_h1_2_w, conv_h1_2_b = mp.conv2d('g_conv_h1_2', h1,
+                                                            [5, 5, h1.get_shape()[-1], self.first_conv_dim * 8],
+                                                            [self.first_conv_dim * 8],
+                                                            [1, 1, 1, 1],
+                                                            padding='SAME', reuse=reuse, with_w=True,
+                                                            trainable=trainable)
+            h1_2 = tf.nn.relu(self.g_bn1_2(conv_h1_2, trainable=trainable))
+
+            # deconv2 conv2d_transpose arguments = (scope_name, inputs, shape, output_shape, bias_shape, stride, padding='VALID', reuse=False, trainable=True)
+            deconv_h2, deconv_h2_w, deconv_h2_b = mp.conv2d_transpose('g_deconv_h2', h1_2,
+                                                                      [5, 5, self.first_conv_dim * 4,
+                                                                       h1_2.get_shape()[-1]],
+                                                                      [self.batch_size, 16, 16,
+                                                                       self.first_conv_dim * 4],
+                                                                      [self.first_conv_dim * 4], [1, 2, 2, 1],
+                                                                      padding='SAME', reuse=reuse, with_w=True)
+            print(deconv_h2.get_shape())
+            h2 = tf.nn.relu(self.g_bn2(deconv_h2, trainable=trainable))
+            # h2 = tf.nn.relu(mp.batch_norm(deconv_h2, scope_name='g_bn_h2', reuse=reuse, trainable=trainable))
+
+            # 2nd conv
+            conv_h2_2, conv_h2_2_w, conv_h2_2_b = mp.conv2d('g_conv_h2_2', h2,
+                                                            [5, 5, h2.get_shape()[-1], self.first_conv_dim * 4],
+                                                            [self.first_conv_dim * 4],
+                                                            [1, 1, 1, 1],
+                                                            padding='SAME', reuse=reuse, with_w=True,
+                                                            trainable=trainable)
+            h2_2 = tf.nn.relu(self.g_bn2_2(conv_h2_2, trainable=trainable))
+
+            # 3rd
+            deconv_h3, deconv_h3_w, deconv_h3_b = mp.conv2d_transpose('g_deconv_h3', h2_2,
+                                                                      [5, 5, self.first_conv_dim * 2,
+                                                                       h2_2.get_shape()[-1]],
+                                                                      [self.batch_size, 32, 32,
+                                                                       self.first_conv_dim * 2],
+                                                                      [self.first_conv_dim * 2], [1, 2, 2, 1],
+                                                                      padding='SAME', reuse=reuse, with_w=True)
+            h3 = tf.nn.relu(self.g_bn3(deconv_h3, trainable=trainable))
+            # h3 = tf.nn.relu(mp.batch_norm(deconv_h3, scope_name='g_bn_h3', reuse=reuse, trainable=trainable))
+
+            # 3rd conv
+            conv_h3_2, conv_h3_2_w, conv_h3_2_b = mp.conv2d('g_conv_h3_2', h3,
+                                                            [5, 5, h3.get_shape()[-1], self.first_conv_dim * 2],
+                                                            [self.first_conv_dim * 2],
+                                                            [1, 1, 1, 1],
+                                                            padding='SAME', reuse=reuse, with_w=True,
+                                                            trainable=trainable)
+            h3_2 = tf.nn.relu(self.g_bn3_2(conv_h3_2, trainable=trainable))
+
+            # 4th
+            deconv_h4, deconv_h4_w, deconv_h4_b = mp.conv2d_transpose('g_deconv_h4', h3_2,
+                                                                      [5, 5, 3, h3_2.get_shape()[-1]],
+                                                                      [self.batch_size, 64, 64, 3],
+                                                                      [3], [1, 2, 2, 1],
+                                                                      padding='SAME', reuse=reuse, with_w=True)
+
+            return tf.nn.tanh(deconv_h4)
+
+
 class Generator:
     def __init__(self, batch_size, first_conv_dim):
         self.batch_size = batch_size
@@ -315,6 +494,7 @@ class DescriminatorExpand:
 
             # linear projection (skip h3)
             h4 = mp.linear_project('d_lin_project_h4', tf.reshape(e_fc1, [self.batch_size, -1]), 1, reuse=reuse)
+            # return tf.nn.tanh(h4), h4, e_fc1
             return tf.nn.sigmoid(h4), h4, e_fc1
 
 
